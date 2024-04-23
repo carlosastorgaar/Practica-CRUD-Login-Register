36b31099d1191244883b270dce2f160a95f0c797
carlosastorgaar <carlosastorga0101@gmail.com>
2024-04-22 21:12:58 -0300

    Probando

--- app/controllers/authentication.controllers.js
+++ app/controllers/authentication.controllers.js
@@ -1,3 +1,12 @@
+import bcryptjs from "bcryptjs";
+
+const usuarios = [{
+    user: "Carlos",
+    email: "carlosastorga0101@gmail.com",
+    password: "$2a$05$GFB1RXgPP1RXgsd5MNmV8emFF4z1aQ.DMxlD4A2Plg/DIOtwL4nZC"
+
+}]
+
 async function login(req, res){
 
 }
@@ -7,7 +16,25 @@ async function register(req, res){
     const user = req.body.user;
     const email = req.body.email;
     const password = req.body.password;
-
+    if (!user || !email || !password) {
+        res.status(400).send({
+            status:"Error", 
+            message:"Los campos estan incompletos"})
+    }
+    const usuarioARevisar = usuarios.find(usuario => usuarios.user === user);
+    if (usuarioARevisar) {
+        res.status(400).send({
+            status:"Error", 
+            message:"Este usuario ya existe"})
+    } 
+    const salt = await bcryptjs.genSalt(5);
+    const hashpassword = await bcryptjs.hash(password, salt);
+    const nuevoUsuario  = {
+        user, email, password: hashpassword
+    }
+    usuarios.push(nuevoUsuario),
+    console.log(usuarios);
+    res.status(201).send({status: "ok", message: 'Usuario ${nuevoUsuario.user} agregado', redirect:"/"})
 }
 
 export const methods = {
